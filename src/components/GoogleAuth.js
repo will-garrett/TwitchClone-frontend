54700c2f39e4a633632fb158863492c673ab7aac
Will Garrett <will.garrett2k@gmail.com>
2019-05-25 23:40:36 -0700

    Basic Google Component

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component{
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', ()=>{
+      window.gapi.client.init({
+        clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        scope: 'email'
+      }).then(()=>{
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.onAuthChange(); // initializes the state call for Google oAuth
+        this.auth.isSignedIn.listen(this.onAuthChange);
+      });
+    });
+  }
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get()});
+  }
+  
+  onSignInClick = () => {
+    this.auth.signIn();
+  }
+  
+  onSignOutClick = ()=>{
+    this.auth.signOut();
+  }
+  
+   
+  renderAuthButton(){
+    if(this.state.isSignedIn === null){
+      return null;
+    }
+    else if(this.state.isSignedIn){
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon"></i>
+          Sign Out
+        </button>
+      )
+    }
+    return (
+      <button onClick={this.onSignInClick} className="ui blue google button">
+        <i className="google icon"></i>
+        Sign In w/Google
+      </button>
+    )
+  }
+  render(){
+    return (
+      <div>{this.renderAuthButton()}</div>
+    )
+  }
+
+}
+export default GoogleAuth;
