6f70350bd4df7a6208b8a9e1366938bfc6789ec7
Will Garrett <will.garrett2k@gmail.com>
2019-05-26 23:11:11 -0700

    Streaming Local Content

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 import { fetchStream } from '../../actions';
+import flv from 'flv.js';
 
 class StreamShow extends React.Component{
-  componentDidMount(){
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props){
+    super(props);
+    this.videoRef = React.createRef();
   }
+  componentDidMount(){
+    
+    let {id} = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+  buildPlayer(){
+    console.log("Player", this.player, "Prop Stream", !this.props.stream);
+    if(this.player || !this.props.stream){
+      console.log("hit exit");
+      return;
+    }
+    let {id} = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    console.log(this.videoRef.current);
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
+  componentDidUpdate(){
+    this.buildPlayer();
+  }
+  
   render(){
     if(!this.props.stream){
       return <div>Loading...</div>
@@ -14,6 +41,7 @@ class StreamShow extends React.Component{
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video style={{width: '100%'}} controls ref={this.videoRef}/>
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
